7a880e53db5561d865f5b1bcf34e6de716795acb
Selena Abjadpour <seab1000@stud.ek.dk>
2025-10-31 15:15:19 +0100

    changes

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.querySelector("#en knap du skal evt skal tilføje i html").addEventListener("click", updateInfoboxHead);
+// Denne JS gøre sådan at når man klikker på knappen, så vil en infobox komme frem på click
+
+function updateInfoboxHead() {
+  console.log("updateinfohead");
+}
