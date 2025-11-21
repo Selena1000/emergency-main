bb34ce39ce84e7a6c5ebeb1121ea3a97e6b54130
Selena Abjadpour <seab1000@stud.ek.dk>
2025-11-21 18:30:33 +0100

    layout ændringer & toggle forbliver det samme når man skifter side

--- js/toggle.js
+++ js/toggle.js
@@ -1,8 +1,19 @@
 const root = document.querySelector("html");
 const btn = document.querySelector("button");
 
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme === "dark") {
+  root.classList.add("dark");
+}
+
 function toogleDarkMode() {
   root.classList.toggle("dark");
+
+  if (root.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
 }
 
 btn.addEventListener("click", toogleDarkMode);
